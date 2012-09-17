7711697babf07716653c7ab4589b56c3d8eb8e58
erashid <ehsan.rashid@gmail.com>
2012-09-17 18:10:25 +0500

    -

---
+++ jquery.gmapthumb.example.js
@@ -0,0 +1,28 @@
+﻿var gMapThumbExample = {};
+(function ($)
+{
+    function loadScript(url)
+    {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = url;
+        document.body.appendChild(script);
+    }
+
+    loadScript('http://maps.google.com/maps/api/js?sensor=false&callback=gMapThumbExample.mapLoaded');
+
+    gMapThumbExample.mapLoaded = function ()
+    {
+        var mapOptions = {
+            zoom: 19,
+            center: new google.maps.LatLng(34.0796167, -118.2585096),
+            mapTypeId: 'hybrid'
+        };
+
+        var map1 = new google.maps.Map($('#map1')[0], mapOptions);
+        var map2 = new google.maps.Map($('#map2')[0], mapOptions);
+
+        map2.setCenter(new google.maps.LatLng(30.3375956, -97.7892168))
+        $('#map1,#map2').gmapthumb([map1, map2]);
+    }
+}(jQuery));
